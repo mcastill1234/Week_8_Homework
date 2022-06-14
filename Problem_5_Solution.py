7a1e8c7f1f270a47bcee329d882491683ad0298f
Mario Castillo <marcas2006@gmail.com>
2022-06-14 08:39:50 -0400

    Uploading problem 5 first tries

--- Problem_5_Solution.py
+++ Problem_5_Solution.py
@@ -1,5 +1,14 @@
 from code_for_hw8_keras import *
-from keras.layers import Conv1D, MaxPooling1D
+from keras.initializers.initializers_v2 import VarianceScaling
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+train, validation = get_MNIST_data()
+# Problem 5B
+layers = [Dense(input_dim=28*28, units=10, activation='softmax')]
+# Problem 5C
+# layers_1 = [Dense(input_dim=28*28, units=10, activation='softmax',
+#                   kernel_initializer=VarianceScaling(scale=0.001, mode='fan_in', distribution='normal',
+#                                                      seed=None),)]
+run_keras_fc_mnist(train, validation, layers, 1, split=0.1, verbose=False, trials=5)
 
